983d161a2638744eb83f893583b8450830c89c53
sasquatchhacking <142119816+sasquatchhacking@users.noreply.github.com>
2025-11-20 20:58:34 -0500

    Update setup.sh

--- scenarios/linux-ctf-4/setup.sh
+++ scenarios/linux-ctf-4/setup.sh
@@ -4,17 +4,6 @@ set -euo pipefail
 # Make a CTF working dir in /root
 mkdir -p /root/ctf
 
-sudo apt-get update -y
-
-sudo apt-get install -y \
-  aircrack-ng \
-  hashcat \
-  hcxtools \
-  wordlists
-
-# Make a CTF working dir in /root
-mkdir -p /root/ctf
-
 # Directory where this script lives
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
@@ -24,3 +13,10 @@ cp "$SCRIPT_DIR/Handshake_02C0CA8D9944.pcap" /root/ctf/Handshake_02C0CA8D9944.pc
 # Lock in sane perms
 chmod 644 /root/ctf/Handshake_02C0CA8D9944.pcap
 
+sudo apt-get update -y
+
+sudo apt-get install -y \
+  aircrack-ng \
+  hashcat \
+  hcxtools \
+  wordlists
